925a0a1090581b4112c348410a3ff052fcf2261b
echos <100727259+echos1234@users.noreply.github.com>
2022-11-01 18:04:19 -0300

    Add files via upload

---
+++ RPS_2.py
@@ -0,0 +1,48 @@
+import random
+
+#Variables
+rps = ["rock", "paper", "scissors"]
+
+winner_dict = {'rock': 'scissors', 'paper': 'rock', 'scissors': 'paper'}
+
+score = 0
+times_played = 0
+question = input("Do you want to play? ")
+
+def update_stats():
+    global score
+    global times_played
+    if winner == 1:
+        score += 1
+
+    elif winner == 2:
+        score = 0
+
+    times_played += 1
+
+while question == "yes".lower():
+    comp_answer = random.choice(rps)
+    human_input = input("Rock, Paper or Scissors? ").lower()
+
+    if winner_dict[human_input] == comp_answer:
+        winner = 1
+        print("You Won")
+
+    elif winner_dict[comp_answer] == human_input:
+        winner = 2
+        print("You Lost")
+
+    elif human_input == comp_answer:
+        winner = 3
+        print("You Tied")
+
+    print("Computer Chose: " + comp_answer)
+
+    update_stats()
+    print("Your Streak Is: " + str(score))
+    print("You've Played: " + str(times_played) + " Times")
+
+    question = input("Again? ")
+
+    if question == "no".lower():
+        quit()
